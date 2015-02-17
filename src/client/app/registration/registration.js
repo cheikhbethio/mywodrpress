c9169e6e4566dfc5efaa71c19b5db5b21d4957de
Sam <samos2@gmail.com>
2015-02-17 18:38:56 +0100

    [refactoring] Mise a jour de toute les references a 'inscription'

---
+++ src/client/app/registration/registration.js
@@ -0,0 +1,48 @@
+'use strict';
+
+
+angular.module('myWordPress.registration', ['ui.router'])
+
+.config(['$stateProvider', '$urlRouterProvider', 
+	function($stateProvider, $urlRouterProvider){
+
+	$stateProvider.state('registration', {
+		url: '/registration',
+		templateUrl: 'registration/registration.html',
+		controller: 'registrationController'
+	});
+
+}])
+
+.controller('registrationController', ['$scope', 
+	function($scope){
+
+		$scope.newUser = {};
+
+		$scope.saveNewUser = function() {
+	  		$scope.$broadcast('show-errors-check-validity');
+
+		  	if ($scope.registrationForm.$valid) {
+		    	console.log("Saving the user");
+		    	console.log($scope.newUser);
+		  	}
+		  	else{
+		  		console.log("Invalide user: ");
+		  		console.log($scope.newUser);
+		  	}
+		};
+
+		$scope.resetRegistrationForm = function() {
+		    $scope.$broadcast('show-errors-reset');
+		    $scope.newUser = {};
+  		};
+
+  		/* For debugging purposes only */
+  		var printNewUser = function(newUser){
+  			console.log("User lastName: " + newUser.lastName);
+  			console.log("User firstName: " + newUser.firstName);
+  			console.log("User email: " + newUser.email);
+  			console.log("User password: " + newUser.password);
+  			console.log("User passwordConfirmation: " + newUser.passwordConfirmation);
+  		};
+}]);
