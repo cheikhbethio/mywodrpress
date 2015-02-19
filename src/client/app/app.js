3276fcb5e4a506e2df1142a41dbc4c36ab20efdc
Sam <samos2@gmail.com>
2015-02-19 15:52:33 +0100

    Ajout du module de modification de profile

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -4,6 +4,7 @@ angular.module('myWordPress', [
 	'myWordPress.registration',
 	'myWordPress.master_template',
 	'myWordPress.connection',
+	'myWordPress.editProfile',
 	'ui.bootstrap.showErrors',
 	'ui.router'
 ])
