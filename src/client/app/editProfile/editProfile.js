9cdf66a995cb9b9d4181feffb1f4e2dab55a8d2f
Sam <samos2@gmail.com>
2015-02-24 15:46:16 +0100

    Rajout du formulaire d'edition de profil et de son controller

--- src/client/app/editProfile/editProfile.js
+++ src/client/app/editProfile/editProfile.js
@@ -13,6 +13,62 @@ angular.module('myWordPress.editProfile', ['ui.router'])
 
 }])
 
-.controller('editProfileController', ['$scope', function($scope){
+.controller('editProfileController', ['$scope', '$rootScope', function($scope, $rootScope){
+
+	$scope.profile = {
+		firstname: 'test_fname',
+		lastname: 'test_lname',
+		login: 'test_log',
+		email: 'test_email@test.com',				
+		password: 'testpass'
+	};
+
+	console.log("$rootScope.userID: " + $rootScope.userID);
+
+
+	$scope.loginAlreadyUsed = false;
+	$scope.emailAlreadyUsed = false;
+
+
+	$scope.saveNewUser = function() {
+
+		  	if ($scope.registrationForm.$valid) {
+
+		    	if($scope.newUser.password === $scope.newUser.passwordConfirmation){
+
+		    		var nuser =  {
+						firstname: $scope.newUser.firstName,
+						lastname: $scope.newUser.lastName, 
+						login: $scope.newUser.login,
+						email: $scope.newUser.email,
+						password: $scope.newUser.password
+					};
+
+		    		user.save(nuser, function(resp) {
+						if(resp.error == 0){
+							console.log("Successfuly posted: " + resp.error);
+							$state.go('app');
+						}else if( resp.error == 1){
+							$scope.emailAlreadyUsed = true;
+						}else if(resp.error == 2){
+							$scope.loginAlreadyUsed = true;
+						}
+
+					}, function(error){
+						console.log("Response: " + error);
+					});
+
+		    	}
+		    	else{
+		    		$scope.nonMatchingPwd = true;
+		    		console.log("the passwords don't match !!");
+		    	}
+		  	}
+		  	else{
+		  		console.log("Invalide form: ");
+		  	}
+
+		  	console.log($scope.newUser);
+		};
 
 }]);
