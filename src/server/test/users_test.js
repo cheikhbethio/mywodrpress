d1b6a23cf26f5f233357a60e6ed2f1d40e967177
Moussa Sow <mmoussasow@gmail.com>
2015-02-26 13:08:05 +0100

    test des routes: afficher pages creer page et editer un page

--- src/server/test/users_test.js
+++ src/server/test/users_test.js
@@ -1,4 +1,4 @@
-var should = require('should'); 
+/*var should = require('should'); 
 var assert = require('assert');
 var request = require('supertest');  
 var mongoose = require('mongoose');
@@ -148,7 +148,7 @@ describe('Routing', function() {
   });
 
 
-
+*/
 
 
 
