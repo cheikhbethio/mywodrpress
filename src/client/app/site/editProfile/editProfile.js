d694385a2b44fbe95e6d128e4209d594e5ec4c3d
Sam <samos2@gmail.com>
2015-02-26 15:08:38 +0100

    Edition du profil: correction du ng-disabled

--- src/client/app/site/editProfile/editProfile.js
+++ src/client/app/site/editProfile/editProfile.js
@@ -16,13 +16,7 @@ angular.module('myWordPress.editProfile', ['ui.router'])
 .controller('editProfileController', ['$scope', '$rootScope', 'User', '$localStorage', 
 	function($scope, $rootScope, User, $localStorage){
 
-	$scope.profile = $localStorage.currentUser;
-	/*$scope.profile = {
-		firstname: 'firectname_uuxu',
-		lastname: 'lastName_uuxu', 
-		login: 'login_uuxu',
-		email: 'email_uuxu@uxu',
-	};*/
+	$scope.profile = angular.copy($localStorage.currentUser);
 
 	console.log("USER editprofile: " + $scope.profile);
 
@@ -32,24 +26,35 @@ angular.module('myWordPress.editProfile', ['ui.router'])
 		login: 'Login',
 		email: 'Email'
 	};
-	
 
 	$scope.loginAlreadyUsed = false;
 	$scope.emailAlreadyUsed = false;
 
 	$scope.editMode = false;
 	$scope.edit_index = false;
+	$scope.edit_save = false;
 
 	$scope.editInformation = function(index, key){
 		console.log("key: " + key);
 		console.log("found index: " + $scope.profile[key]);
 		$scope.editMode = true;
 		$scope.edit_index = index;
+		$scope.edit_save = angular.copy($scope.profile[key]);
 	};
 
-	$scope.cancelEditInformation = function(){
+	$scope.cancelEditInformation = function(key){
+		$scope.profile[key] = $scope.edit_save;
+		$scope.reset();
+	};
+
+	$scope.saveEditInformation = function(key){
+		$scope.reset();
+	};
+
+	$scope.reset = function(){
 		$scope.editMode = false;
 		$scope.edit_index = false;
+		$scope.edit_save = false;
 	};
 
 	$scope.isEditMode = function(index) {
@@ -61,45 +66,27 @@ angular.module('myWordPress.editProfile', ['ui.router'])
 	}
 
 
-	$scope.saveNewUser = function() {
+	$scope.saveUser = function() {
 
-	  	if ($scope.registrationForm.$valid) {
+		User.update($scope.profile, function(resp) {
 
-	    	if($scope.newUser.password === $scope.newUser.passwordConfirmation){
+			if(resp.error == 0){
+				console.log("Successfuly posted: " + resp.error);
+				$state.go('app');
+			} 
+			else if( resp.error == 1){
+				$scope.emailAlreadyUsed = true;
+			} 
+			else if(resp.error == 2){
+				$scope.loginAlreadyUsed = true;
+			}
+			else if(resp.error == 3){
+				console.log("Bad ID !!");
+			}
 
-	    		var edited_profile =  {
-					firstname: $scope.profile.firstName,
-					lastname: $scope.profile.lastName, 
-					login: $scope.profile.login,
-					email: $scope.profile.email,
-					password: $scope.profile.password
-				};
-
-	    		User.save(edited_profile, function(resp) {
-					if(resp.error == 0){
-						console.log("Successfuly posted: " + resp.error);
-						$state.go('app');
-					} else if( resp.error == 1){
-						$scope.emailAlreadyUsed = true;
-					} else if(resp.error == 2){
-						$scope.loginAlreadyUsed = true;
-					}
-
-				}, function(error){
-					console.log("Response: " + error);
-				});
-
-	    	}
-	    	else{
-	    		$scope.nonMatchingPwd = true;
-	    		console.log("the passwords don't match !!");
-	    	}
-	  	}
-	  	else{
-	  		console.log("Invalide form: ");
-	  	}
-
-	  	console.log($scope.newUser);
+		}, function(error) {
+			console.log("Response: " + error);
+		});
 	};
 
 }]);
