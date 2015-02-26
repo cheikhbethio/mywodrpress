9ad8e2beaa6a57918313c242a76eb3d5b90ae724
Sadio Koma <komasadio@gmail.com>
2015-02-26 16:42:41 +0100

    test back-end des pages et des menus

---
+++ src/server/test/menu_test.js
@@ -0,0 +1,66 @@
+var should = require('should'); 
+var assert = require('assert');
+var request = require('supertest');  
+var mongoose = require('mongoose');
+
+describe('Routing', function() {
+  var url = 'http://localhost:4711';
+ /*before(function(done) {
+    // In our tests we use the test db
+    function clearDB() {
+   for (var i in mongoose.connection.collections) {
+     mongoose.connection.collections[i].remove(function() {});
+   }
+   return done();
+ }
+    mongoose.connect('localhost:27017');	
+    clearDB();
+    done();
+  });*/
+
+describe('Create a new menu', function(){
+        var resBody;
+       it('should create a new menu', function(done){
+         var menu = {name: 'Menu de test', items: []};
+          request(url)
+        .post('/api/menus')
+        .send(menu)
+        .expect(200)
+        .end(function(err,res){
+         should.not.exist(err);
+         console.log(res.body);
+         resBody = res.body;
+         done();
+      });
+   });
+  it('should add page to a menu', function(done){
+      var page = {title: 'test', content: 'Ceci est un test',pageId: '54ef39793f8dac1713838a83'};
+      var id = resBody._id;
+      request(url)
+      .post('/api/menus/'+id)
+      .send(page)
+      .expect(200)
+     .end(function(err, res){
+       should.not.exist(err);
+         //res.body.title.should.equal('modification du titre');
+         //res.body.content.should.equal('test de la modification du contenu');
+         done()
+      });
+  });
+});
+describe('Get all menus', function(){
+        it('should return liste de tous les menus',function(done){
+          request(url)
+          .get('/api/menus')
+          .set('Accept','application/json')
+          .expect(200)
+          .end(function(err,res){
+           should.not.exist(err);
+           res.body.should.be.an.instanceOf(Array);
+            console.log(res.body);
+          done();
+        });
+      });
+});
+});
+      
---
+++ src/server/test/page_test.js
@@ -0,0 +1,85 @@
+var should = require('should'); 
+var assert = require('assert');
+var request = require('supertest');  
+var mongoose = require('mongoose');
+
+ 
+
+
+describe('Routing', function() {
+  var url = 'http://localhost:4711';
+  before(function(done) {
+    // In our tests we use the test db
+    function clearDB() {
+   for (var i in mongoose.connection.collections) {
+     mongoose.connection.collections[i].remove(function() {});
+   }
+   return done();
+ }
+    mongoose.connect('localhost:27017');	
+    clearDB();
+    done();
+  });
+
+describe('Create a new Page',function(){
+         var resBody;
+        it('should create a new page', function(done){
+        var page = {title: "test", content: "Ceci est un test"};
+        request(url)
+        .post('/api/pages')
+        .send(page)
+        .expect(200)
+        .end(function(err,res){
+         should.not.exist(err);
+         resBody = res.body;
+         console.log(res.body);
+         done();
+      });
+ });
+  it('should update an existing page', function(done){
+     var body = {title: "modification du titre", content: "test de la modification du contenu"};
+     var id = resBody._id;
+     request(url)
+     .put('/api/pages/'+id)
+     .send(body)
+     .expect(200)
+     .end(function(err, res){
+       should.not.exist(err);
+         res.body.title.should.equal('modification du titre');
+         res.body.content.should.equal('test de la modification du contenu');
+         done();
+      
+    });
+  });
+ it('should delete an existing page', function(done){
+  var id = resBody._id;
+  request(url)
+  .delete('/api/pages/'+id)
+  .expect(200)
+  .end(function(err,res){
+   should.not.exist(err);
+   console.log(res.body);
+   done();
+       
+});
+});
+});
+
+  
+describe('Get Pages', function(){
+         it('should return liste de toutes les pages',function(done){
+          request(url)
+          .get('/api/pages')
+          .set('Accept','application/json')
+          .expect(200)
+          .end(function(err,res){
+           should.not.exist(err);
+           res.body.should.be.an.instanceOf(Array);
+            console.log(res.body);
+           res.body[0].title.should.equal("test");
+           res.body[0].content.should.equal("Ceci est un test");
+          done();
+        });
+      });
+});
+});
