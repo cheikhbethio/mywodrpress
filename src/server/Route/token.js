d6ea8d18623079bfb733bbf2083915851c3f6e3e
nikko456 <nicolasspagnulo@gmail.com>
2015-02-27 12:17:19 +0100

    Fix edit back-end, + renvoi user créer dasn la réponse

--- src/server/Route/token.js
+++ src/server/Route/token.js
@@ -1,8 +1,34 @@
 var user=require('./user.js');
 var jwt=require('jwt-simple');
 var moment=require('moment');
+var mongoose=require('mongoose')
+var secret_Token;
+var modelUser=user.users;
+moment.format();
 
 
-module.exports.auth=function(req,res,next){
 
-var token=req.headers['x-access-token'];
+module.exports.login=function(req,res,next){
+var login=req.body.login;
+var password=req.body.password;
+
+modelUser({login: login},function(err, doc){
+
+	if(err){
+		res.sendStatus(404,err.message);
+		next();
+	}
+	else if(!doc){
+		res.sendStatus(401,"User not Found");
+		next();
+	}
+	else{
+		var expires=moment().add('hours',2);
+		var token = jwt.encode({
+			iss: user.id,
+			exp: expires
+		}, secret_Token);
+		res.send({token : token, expires : expires});
+
+	}
+}
--- src/server/Route/user.js
+++ src/server/Route/user.js
@@ -67,7 +67,7 @@ exports.create=function (req, res , next) {
             res.sendStatus(401,{error : err.message});
         }
         else
-            res.send({error : 0});
+            res.send({error : 0, result results});
     });
 };
 
@@ -79,43 +79,44 @@ exports.edit = function (req, res , next) {
     var query= ({_id : req.params.id});
  		var maj={};
     user.findById(req.params.id, function(error,result){
-                        if(error) next(error);
-                        else if(result==null){
-                        console.log("user not found when editing");
-                        res.sendStatus(404, {error : "0"});
-                        next();                        
-                        }                        
-                        else
-                        	user.findOne({email : req.body.email}, 'email', function(errt, doc) {
-                        		if(errt) next(errt);
-                        		else{
-                        			if(result.email!=req.body.email){
-																	if(doc) {
-           													res.sendStatus(401,{error : "1"});
-           													next();           													
-       												 		}else{
-       												 			if(req.body.email!=null)
-       												 				maj.email=req.body.email;
-        														if(req.body.password!=null)
-        															maj.password=bcrypt.hashSync(req.body.password, 8);
-        														if(req.body.firstname!=null)
-      										 						maj.firstname=req.body.firstname;
-      										 					if(req.body.lastname!=null)
-      											 					maj.lastname=req.body.lastname;
-    	       													console.log("maj",maj);
-		 																	user.update(query, maj,function(errs,n){
-               							 							if(errs) 
-                    												next(errs);
-                    											else
-                    												res.send({error : "0"});
-                    												next();
-    																		});
-    															}
-    														}
-    													}
-    									});
- });
-
+        if(error) next(error);
+        else if(result==null){
+            console.log("user not found when editing");
+            res.sendStatus(404, {error : "0"});
+            next();
+        }
+        else
+            user.findOne({email : req.body.email}, function(errt, doc) {
+                if(errt) next(errt);
+                else{
+                    console.log("req",req.body);
+                    console.log("doca",doc);
+                    if(result.email!=req.body.email){
+                        if(doc) {
+                            console.log("email already use");
+                            res.sendStatus(401,{error : "1"});                            
+                            return next();
+                        }}
+                        if(req.body.email!=null)
+                            maj.email=req.body.email;
+                        if(req.body.password!=null)
+                            maj.password=bcrypt.hashSync(req.body.password, 8);
+                        if(req.body.firstname!=null)
+                            maj.firstname=req.body.firstname;
+                        if(req.body.lastname!=null)
+                            maj.lastname=req.body.lastname;
+                        user.update(query, maj,function(errs,n){
+                            if(errs){
+                                console.log("error when update user");
+                                next(errs);
+                            }
+                            else
+                                res.send({error : "0"});
+                                next();
+                            });
+                    }
+                });
+            });
     
 
 };
@@ -123,10 +124,10 @@ exports.edit = function (req, res , next) {
 exports.get = function(req,res,next){
         var id = req.params.id;
         user.findById(id, function(err,result){
-                        if(err) return next(err);
-                        else
-                            res.json(result);
-                });
+            if(err) return next(err);
+            else
+                res.json(result);
+        });
     };
 
 exports.view = function (req, res ,next) {
