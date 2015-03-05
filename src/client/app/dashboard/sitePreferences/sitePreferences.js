3ee9e27036117d9a6c04001950bb6bde0fc6aac1
Sam <samos2@gmail.com>
2015-03-05 16:18:14 +0100

    Creation du service pour les preferences

--- src/client/app/dashboard/sitePreferences/sitePreferences.js
+++ src/client/app/dashboard/sitePreferences/sitePreferences.js
@@ -22,19 +22,7 @@ angular.module('myWordPress.sitePreferences', ['ui.router'])
 	$scope.preferences = Preferences.get();
 
 	$scope.savePreferences = function(){
-		/*Preferences.save($scope.preferences, function(resp){
-			console.log("Saved preferences");
-		}, function(err){
-			console.log("Could not save preferences");
-		});*/
 
-		$http.put('components/preferences/preferences.json', $scope.preferences).
-		  success(function(data, status, headers, config) {
-		    console.log("Saved preferences");
-		  }).
-		  error(function(data, status, headers, config) {
-		    console.log("Could not save preferences");
-		  });
 	};
 
 	console.log("Preferences : " + $scope.preferences);
