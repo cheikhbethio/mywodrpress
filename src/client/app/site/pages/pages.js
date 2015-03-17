94f373148e7b7f0f03f41bf14ef344dfdcc20618
Moussa Sow <mmoussasow@gmail.com>
2015-03-17 15:57:05 +0100

    bouton des favorite

--- src/client/app/site/pages/pages.js
+++ src/client/app/site/pages/pages.js
@@ -15,8 +15,10 @@ angular.module('myWordPress.pages', ['ui.router'])
 
 .controller('pagesController', ['$scope', '$state', '$stateParams', 'Page','$localStorage', 'AddFavorite', 'Article', function($scope, $state, $stateParams, Page, $localStorage, AddFavorite, Article){
 
-
-    $scope.id_user = $localStorage.currentUser._id;
+	if(typeof $localStorage.currentUser != 'undefined'){
+    	$scope.id_user = $localStorage.currentUser._id;
+    	console.log($scope.id_user + " test id user");
+	}
 
 	$scope.page = Page.get({id: $stateParams.id}, function(page) {
         console.log("get page "+$stateParams.id);
