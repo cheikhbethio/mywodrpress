16c8408aa5eb6077a6fae8e58535e6ee95585093
laloutr3 <laloutr3@hotmail.fr>
2015-03-18 07:48:39 -0700

    Corrections mineures diverses sur les affichages d'alertes de connexion/inscription

--- src/client/app/site/connection/connection.js
+++ src/client/app/site/connection/connection.js
@@ -6,16 +6,17 @@ angular.module('myWordPress.connection', ['ui.router'])
 .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
 
 	$stateProvider.state('site.connection', {
-		url: 'connection',
+		url: 'connection?registration',
 		templateUrl: 'site/connection/connection.html',
 		controller: 'connectionController'
 	});
 
 }])
 
-.controller('connectionController', ['$scope','$http', '$rootScope', '$state', 'Login','Token', "$localStorage", function($scope, $http, $rootScope, $state, Login, Token, $localStorage){
+.controller('connectionController', ['$scope','$http', '$rootScope', '$state', '$stateParams', 'Login','Token', "$localStorage", function($scope, $http, $rootScope, $state, $stateParams, Login, Token, $localStorage){
 	$scope.newUser;
 	$scope.connectionError = false;
+	$scope.registration = $stateParams.registration;
 
 	$scope.connectUser=function(){
   		if ($scope.connectionForm.$valid) {
@@ -56,6 +57,7 @@ angular.module('myWordPress.connection', ['ui.router'])
 
 	$scope.closeAlert = function() {
 		$scope.connectionError = false;
+		$scope.registration = false;
 	};
 
 }]);
