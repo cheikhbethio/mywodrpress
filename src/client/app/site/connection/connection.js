bf99bf3f9c3746893ee2d65384d038b1aacf1fd4
Sam <samos2@gmail.com>
2015-03-20 11:31:51 +0100

    Modification de la mise en form du controller de la connection ainsi que la suppression de variables inutiles. Surtout, set du header http au niveau de la connection.

--- src/client/app/site/connection/connection.js
+++ src/client/app/site/connection/connection.js
@@ -13,34 +13,36 @@ angular.module('myWordPress.connection', ['ui.router'])
 
 }])
 
-.controller('connectionController', ['$scope','$http', '$rootScope', '$state', '$stateParams', 'Login','Token', "$localStorage", function($scope, $http, $rootScope, $state, $stateParams, Login, Token, $localStorage){
-	$scope.userCredentials = {};
-	$scope.connectionError = false;
-	$scope.registration = $stateParams.registration;
-
-	$scope.connectUser=function(){
-  		if ($scope.connectionForm.$valid) {
-
-			Token.login($scope.userCredentials, function(res){
-
-					$localStorage.accessToken = res.token;
-					$localStorage.currentUser = res.user;
-
-					$state.go('site.home', {connectionSuccess:true});
-
-				}, function(err){
-					console.log('Could not get token: ' + err.data.message);
-			});
-		}
-		else {
-			console.log('Formulaire Invalide.');
-			$scope.connectionError = true;
-		}
-	}
-
-	$scope.closeAlert = function() {
+.controller('connectionController', ['$scope', '$rootScope', '$state', '$stateParams', '$http', '$localStorage', 'Token',
+	function($scope, $rootScope, $state, $stateParams, $http, $localStorage, Token){
+	
+		$scope.userCredentials = {};
 		$scope.connectionError = false;
-		$scope.registration = false;
-	};
+		$scope.registration = $stateParams.registration;
+
+		$scope.connectUser=function(){
+	  		if ($scope.connectionForm.$valid) {
+
+				Token.login($scope.userCredentials, function(res){
+
+						$localStorage.currentUser = res.user;
+						$http.defaults.headers.common['x-access-token'] = res.token;
+
+						$state.go('site.home', {connectionSuccess:true});
+
+					}, function(err){
+						console.log('Could not get token: ' + err.data.message);
+				});
+			}
+			else {
+				console.log('Formulaire Invalide.');
+				$scope.connectionError = true;
+			}
+		}
+
+		$scope.closeAlert = function() {
+			$scope.connectionError = false;
+			$scope.registration = false;
+		};
 
 }]);
